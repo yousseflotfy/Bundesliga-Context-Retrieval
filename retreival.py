10d6c36d8b63d151ebe0779f78f73df735701c72
yousseflotfy <youssef@Youssefs-MacBook-Air.local>
2025-10-23 18:13:17 +0300

    fix: Exceptions Handled

--- retreival.py
+++ retreival.py
@@ -3,6 +3,12 @@ import spacy
 import re
 import wikipediaapi
 
+class InvalidInputError(Exception):
+    """Custom exception for invalid user input city."""
+
+class WikiAPIError(Exception):
+    """Custom exception for invalid user input city."""
+
 def bundesliga_clubs_retreival():
     """
     Function to query wikidata and construct a dict of cities 
@@ -25,17 +31,29 @@ def bundesliga_clubs_retreival():
     for res in results:
         match = re.search(r'entity/(.*)', res["club"]["value"])
         if not match:
-            raise Exception("club not found")
+            raise LookupError("club not found")
         cities_clubs_dict[res["cityLabel"]["value"]] = match.group(1)
     return cities_clubs_dict
 
 def question_city_extraction(question:str,cities_clubs_dict:dict):
+    """
+    Function to extract city from question and retreive its club .
+    input question, dict
+    returns club
+    """
     for city in cities_clubs_dict.keys():
         pattern = r'\b' + re.escape(city) + r'\b'
-        if re.search(pattern,question,flags=re.IGNORECASE):
-            return city, cities_clubs_dict.get(city)
+        if  re.search(pattern,question,flags=re.IGNORECASE):
+            return cities_clubs_dict.get(city)
+    raise InvalidInputError("Please Enter a valid city with a team playing in Bundesligue 1.")
 
 def club_coach_retreival(club:str):
+    """
+    Function to query wikidata and to find a club coach 
+    args: club
+    returns: str
+    """
+
     sparql_query = """
     SELECT DISTINCT ?coach ?coachLabel
     WHERE
@@ -51,17 +69,22 @@ def club_coach_retreival(club:str):
     return results
 
 def coach_info_retreival(coach:str):
+    """
+    function that calls wikipedia api to retreive coach info
+    args: coach ast
+    returns : str
+    """
     wiki_wiki = wikipediaapi.Wikipedia(user_agent='Bundesliga-Context-Retrieval', language='en')
     page_py = wiki_wiki.page(coach)
     if not page_py.exists():
-        raise Exception("Wikipedia Page Not Found")
-    # print(page_py.summary)
+        raise WikiAPIError("Wikipedia Page Not Found. Please try again!")
     return page_py.summary
 
-def wrapper(question:str,city_club_dict):
-    city,club = question_city_extraction(question,city_club_dict)
-    coach = club_coach_retreival(club)
-    return coach_info_retreival(coach)
+# def wrapper(question:str,city_club_dicti):
+
+#     club = question_city_extraction(question,city_club_dicti)
+#     coach = club_coach_retreival(club)
+#     return coach_info_retreival(coach)
 
 
 if __name__ == "__main__":
@@ -76,8 +99,13 @@ if __name__ == "__main__":
             break
         
         # simple response logic
-        response = wrapper(user_input,city_club_dict)
-        print(f"Chatbot: {response}")
+        try:
+            club = question_city_extraction(user_input,city_club_dict)
+            coach = club_coach_retreival(club)
+            print(f"Chatbot: { coach_info_retreival(coach)}")
+        except InvalidInputError as e:
+            print(f"Chatbot: {e}")
+
 
 # print(club_coach_retreival("wd:Q15789"))
 # print(question_city_extraction("Who is coaching Munich?",bundesliga_clubs_retreival().keys()))
